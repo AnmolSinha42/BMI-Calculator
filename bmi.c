006af49e567310f855f4cde2168c0c884a1f6132
anmol <anmolsinhaiit@gmail.com>
2024-07-11 19:12:36 +0530

    Completed!

--- bmi.c
+++ bmi.c
@@ -13,11 +13,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 
 int userinput(int initial, char* text,char* unit);
 float calculate(int weight, int height);
 int *range(int weight, int height, int wrange, int hrange);
+void find_ideal(int weight, int height, int bmi);
 
 int main(void)
 {
@@ -28,7 +30,7 @@ int main(void)
 
     //range (arbitary)
     const int wrange = 2;
-    const int hrange = 2;
+    const int hrange = 5;
 
     //calculate
     float bmi = calculate(weight, height);
@@ -52,10 +54,59 @@ int main(void)
     free(limits);
 
     //status
-    printf("BMI: %f\n",bmi);
+    printf("BMI: %.2f\n",((int)(bmi*10))/10.0); // 2f for 2 decimal places
     printf("Range: %i-%i\n",llimit, rlimit);
+    printf("\n");
 
-    
+    int index;
+    int values[10] = {0,16,17,18,25,30,35,40,1000};
+    for (int i=0; i<9; i++){
+        if (bmi<=values[i]){
+            index = i;
+            break;
+        }
+    }
+    //iterate all possiblities
+    printf("Chart:\n");
+    if (index <= 2){
+        printf("Severe Thinness : <16\n");
+    }
+    if (index <= 3){
+        printf("Moderate Thinness : 16-17\n");
+    }
+    if ((index > 1)&&(index < 5)){
+        printf("Mild Thinness : 17-18\n");
+    }
+    if ((index > 2)&&(index < 6)){
+        printf("Normal : 18-25\n");
+    }
+    if ((index > 3)&&(index < 7)){
+        printf("Overweight : 25-30\n");
+    }
+    if ((index > 4)&&(index < 8)){
+        printf("Obese Class I : 30-35\n");
+    }
+    if ((index > 5)&&(index < 9)){
+        printf("Obese Class II : 35-40\n");
+    }
+    if ((index > 6)&&(index < 10)){
+        printf("Obese Class III : >40\n");
+    }
+    printf("\n");
+
+    //improve (BMI - 22)
+    //let height be normal find weight and vice versa
+
+    const int ideal = 22;
+    if ((int)(bmi+0.5) == ideal){
+        printf("You have a perfect BMI! Keep it up!");
+    }
+    else{
+        find_ideal(weight, height, ideal);
+    }
+
+    printf("\n");
+
 }
 
 int userinput(int initial, char* text,char* unit){
@@ -90,6 +141,21 @@ int *range(int weight, int height, int wrange, int hrange){
     rbmi[2] = calculate(weight-wrange, height);
     rbmi[3] = calculate(weight, height+hrange);
     return rbmi;
+}
+
+void find_ideal(int weight, int height, int bmi){
+    float wimprove;
+    wimprove = weight - (bmi*((float)(height*height)/10000));
+    printf("Need to ");
+    if (wimprove>0){
+        printf("Lose");
+    }
+    else{
+        printf("Gain");
+        wimprove = wimprove*(-1);
+    }
+    printf(" %.2f kg\n",((int)(wimprove*10))/10.0);
+
 }
 
 
